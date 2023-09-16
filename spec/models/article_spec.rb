43e8f99a9937a329afe9d80a134dd88241e2c81f
Rinat273 <bairam0487@gmail.com>
2023-09-16 04:08:39 +0400

    Update article spec to include validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,8 @@ describe Article do
   describe "validations" do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
+    it { should validate_length_of(:title).is_at_most(140) }
+    it { should validate_length_of(:text).is_at_most(4000) }
   end
 
   describe "associations" do
@@ -18,7 +20,9 @@ describe Article do
       # assert, проверка
       expect(article.subject).to eq 'Lorem Ipsum'
 
-    end  
+    end 
+
+
   end
 
   describe "#last_comment" do
