d18ef9ba54a4c1b0b3123b8155d58ab24c729c36
Rinat273 <bairam0487@gmail.com>
2023-09-17 01:17:17 +0400

    Update article tests (allows a quest to create article)

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -9,5 +9,15 @@ feature "Article Creation" do
     visit new_article_path
     expect(page).to have_content I18n.t "articles.new_article"
   end
+  
+  scenario "allows a quest to create article" do
+    visit new_article_path
+    
+    fill_in :article_title, :with => 'first article'
+    fill_in :article_text, :with => 'something'
+    click_button 'Save Article'
 
+    expect(page).to have_content 'first article'
+
+  end
 end
